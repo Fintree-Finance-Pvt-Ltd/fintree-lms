853b66bfd61fd13f8d441f845191d9bc05aa3734
Sajag Jain <sajag@example.com>
2025-09-03 16:37:29 +0530

    SA

--- Backend/routes/repaymentsRoutes.js
+++ Backend/routes/repaymentsRoutes.js
@@ -3,36 +3,36 @@ const multer = require("multer");
 const xlsx = require("xlsx");
 const db = require("../config/db");
 const { allocateRepaymentByLAN } = require("../utils/allocate");
-//const { excelSerialDateToJS, queryDB } = require("../utils/helpers");
+const { excelSerialDateToJS, queryDB } = require("../utils/helpers");
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
-// ✅ Excel serial date to JS date (YYYY-MM-DD)
-const excelSerialDateToJS = (value) => {
-  if (!value) return null;
+// // ✅ Excel serial date to JS date (YYYY-MM-DD)
+// const excelSerialDateToJS = (value) => {
+//   if (!value) return null;
 
-  if (!isNaN(value)) {
-    const excelEpoch = new Date(Date.UTC(1899, 11, 30));
-    return new Date(excelEpoch.getTime() + value * 86400000)
-      .toISOString()
-      .split("T")[0];
-  }
+//   if (!isNaN(value)) {
+//     const excelEpoch = new Date(Date.UTC(1899, 11, 30));
+//     return new Date(excelEpoch.getTime() + value * 86400000)
+//       .toISOString()
+//       .split("T")[0];
+//   }
 
-  if (typeof value === "string" && value.match(/^\d{2}-[A-Za-z]{3}-\d{2}$/)) {
-    const [day, monthAbbr, yearShort] = value.split("-");
-    const monthNames = {
-      Jan: 0, Feb: 1, Mar: 2, Apr: 3, May: 4, Jun: 5,
-      Jul: 6, Aug: 7, Sep: 8, Oct: 9, Nov: 10, Dec: 11,
-    };
-    const month = monthNames[monthAbbr];
-    const year = parseInt("20" + yearShort, 10);
-    return new Date(Date.UTC(parseInt(day), month, year))
-      .toISOString()
-      .split("T")[0];
-  }
+//   if (typeof value === "string" && value.match(/^\d{2}-[A-Za-z]{3}-\d{2}$/)) {
+//     const [day, monthAbbr, yearShort] = value.split("-");
+//     const monthNames = {
+//       Jan: 0, Feb: 1, Mar: 2, Apr: 3, May: 4, Jun: 5,
+//       Jul: 6, Aug: 7, Sep: 8, Oct: 9, Nov: 10, Dec: 11,
+//     };
+//     const month = monthNames[monthAbbr];
+//     const year = parseInt("20" + yearShort, 10);
+//     return new Date(Date.UTC(parseInt(day), month, year))
+//       .toISOString()
+//       .split("T")[0];
+//   }
 
-  return null;
-};
+//   return null;
+// };
 
 
 router.post("/upload", upload.single("file"), async (req, res) => {
