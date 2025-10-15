4c7bcab97c78df9d953c362809ee958c7682471b
Sajag Jain <sajag@example.com>
2025-10-15 10:05:06 +0530

    loan_booking_circle_pe add in dis page

--- Backend/routes/DisbursalRoutes.js
+++ Backend/routes/DisbursalRoutes.js
@@ -35,6 +35,19 @@ router.get("/:lan", async (req, res) => {
     partnerLoanIdCol = "lb.partner_loan_id";
     netDisbursementExpr = `(${loanAmountExpr} - ${subventionCol})`;
   }
+ if (lan.startsWith("CIRF")) {
+    tableName = "loan_booking_circle_pe";
+    loanAmountCol = "lb.loan_amount";
+    loanAmountExpr = "lb.loan_amount";
+    interestRateCol = "lb.interest_rate";
+    tenureCol = "lb.loan_tenure";
+    processingFeeCol = "COALESCE(lb.processing_fee, 0) AS processing_fee";
+    subventionCol = "0";
+    retentionCol = "0";
+    partnerLoanIdCol = "lb.partner_loan_id";
+    netDisbursementExpr = `(${loanAmountExpr} - ${subventionCol})`;
+  }
+
 
   if (lan.startsWith("HEYEV")) {
     tableName = "loan_booking_hey_ev";
