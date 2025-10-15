ac038dcdd585d7c9c1484f58072da21a0805e94d
Sajag Jain <sajag@example.com>
2025-10-15 16:00:29 +0530

    SA

--- Frontend/src/components/Reports/TriggerReportForm.jsx
+++ Frontend/src/components/Reports/TriggerReportForm.jsx
@@ -276,7 +276,7 @@ const productOptions = [
   { label: "Adikosh", value: "Adikosh" },
   { label: "CCOD", value: "CC-OD" },
   { label: "Embifi", value: "Embifi" },
-  { lable: "EMICLUB", value: "EMICLUB"}
+  { lable: "EMI CLUB", value: "EMICLUB"}
 ];
 
 const formatDate = (date) => {
