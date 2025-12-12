545da40c8cb78b5b8ee7dff0efdcad67796c93ba
Sajag Jain <sajag.jain@fintreefinance.com>
2025-12-12 12:47:57 +0530

    SA

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 20,
   queueLimit: 0
 });
 
