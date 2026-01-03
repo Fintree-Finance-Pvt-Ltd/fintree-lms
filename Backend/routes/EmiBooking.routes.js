f08e76b5fef4929a70ca6aa170fa85ab8bc74d68
Sajag Jain <sajag.jain@fintreefinance.com>
2026-01-03 18:20:59 +0530

    SASA

--- Backend/routes/EmiBooking.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/loanBooking.controller");
-
-router.put("/approve-initiated-loans/:lan", controller.approveInitiatedLoan);
-
-module.exports = router;
--- Backend/routes/webhook.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/easebuzzWebhook.controller");
-
-router.post("/easebuzz/payout", controller.easebuzzPayoutWebhook);
-
-module.exports = router;
