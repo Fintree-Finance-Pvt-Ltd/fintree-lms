597aa7e81546e53913f478af1126230c8bd3b61a
Sajag Jain <sajag.jain@fintreefinance.com>
2026-02-26 13:07:04 +0530

    SA

--- Backend/routes/utrRoutes.js
+++ Backend/routes/utrRoutes.js
@@ -609,7 +609,7 @@ router.post("/upload-utr", upload.single("file"), async (req, res) => {
   no_of_advance_emis,
   product,
   lender,
-  retention_percent,
+  retention_percentage ,
   manual_retention_amount AS retention_amount
 FROM loan_booking_gq_fsf
 WHERE lan = ?`,
