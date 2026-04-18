c2f4005bc6b12f4b80e0b0628dcfb44e0993d766
Pratik sonawane <pratik.sonawane1022@gmail.com>
2026-04-18 16:01:47 +0530

    fix

--- Frontend/src/components/Loan Digit/LoanDigitAllLoans.jsx
+++ Frontend/src/components/Loan Digit/LoanDigitAllLoans.jsx
@@ -4,7 +4,7 @@ import AllLoansScreen from "../AllLoansScreen";
 const LoanDigitAllLoans = () => {
   return (
     <AllLoansScreen
-      apiEndpoint={`/loan-booking/all-loans?table=loan_booking_loan_digit&prefix=FINE`}
+      apiEndpoint={`/loan-booking/all-loans?table=loan_booking_loan_digit&prefix=LDF`}
       title="Loan Digit All Loans"
     />
   );
--- Frontend/src/components/Loan Digit/LoanDigitApproved.jsx
+++ Frontend/src/components/Loan Digit/LoanDigitApproved.jsx
@@ -4,7 +4,7 @@ import ApprovedLoansTable from "../ApprovedLoansScreen";
 const LoanDigitApproved = () => {
   return (
     <ApprovedLoansTable
-      apiUrl={`/loan-booking/approved-loans?table=loan_booking_loan_digit&prefix=FINE`}
+      apiUrl={`/loan-booking/approved-loans?table=loan_booking_loan_digit&prefix=LDF`}
       title="Loan Digit Approved Loans"
       lenderName="Loan Digit"
     />
