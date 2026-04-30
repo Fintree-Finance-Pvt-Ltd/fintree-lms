2dc9a07e20530787cf1a97af5fcf3ceebb58e998
Sajag Jain <jain.sajag18@gmail.com>
2026-04-30 10:52:18 +0530

    SA

--- Backend/routes/excelUpload.js
+++ Backend/routes/excelUpload.js
@@ -8090,7 +8090,7 @@ console.log("[Invoice Insert] penal_rate:", data.penal_rate);
           disbursement_utr,
           roi_percentage,
           roi_penal_rate,
-          service_Fee,  
+          service_charges,  
           penal_rate,
           total_roi_amount,
           emi_amount
@@ -8115,7 +8115,7 @@ console.log("[Invoice Insert] penal_rate:", data.penal_rate);
           data.disbursement_utr,
           data.roi_percentage,
           data.roi_percentage, // 👈 updated here
-            data.service_charges || 0, // ✅ inserted value here
+            data.service_Fee || 0, // ✅ inserted value here
           data.penal_charges || 0,
           data.total_roi_amount,
           data.emi_amount,
