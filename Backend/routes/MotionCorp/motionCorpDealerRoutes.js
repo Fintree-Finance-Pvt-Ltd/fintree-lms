aa7928a42b8d0daa6ec05777a76abc0e6070382e
joshirohit <joshirohit2004@gmail.com>
2026-05-13 10:37:42 +0530

    route fix of motion corp

--- Backend/routes/MotionCorp/motionCorpDealerRoutes.js
+++ Backend/routes/MotionCorp/motionCorpDealerRoutes.js
@@ -1757,7 +1757,7 @@ router.get("/aadhaar-address/:lan/:applicantType", async (req, res) => {
   }
 });
 
-router.get("/motion-corp/customer-details/:lan", async (req, res) => {
+router.get("/customer-details/:lan", async (req, res) => {
   const { lan } = req.params;
 
   try {
