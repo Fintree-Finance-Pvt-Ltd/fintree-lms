c18cbabea7ba7cd2387b725c82e1cc077a46bc90
Harish-lodh <harishlodh444@gmail.com>
2026-07-03 12:21:08 +0530

    fix report

---
+++ Frontend/src/components/WCTLFFPL/WCTLFFPLApprovedLoans.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ApprovedLoansTable from "../ApprovedLoansScreen";
+
+const WCTLFFPLApprovedLoans = () => (
+  <ApprovedLoansTable
+    apiUrl="/loan-booking/approved-loans?table=loan_booking_wctl_ffpl&prefix=TLF"
+    title="WCTL FFPL Approved Loans"
+    lender="WCTL FFPL"
+  />
+);
+
+export default WCTLFFPLApprovedLoans;
