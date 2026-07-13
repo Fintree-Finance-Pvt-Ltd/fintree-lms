dcd3158740f80241fe887e3594d760399dcda065
jain18sajag <jain.sajag18@gmail.com>
2026-07-13 12:02:08 +0530

    fix: Add query for loan_booking_motion_corp to processRows function

--- Backend/routes/repaymentsRoutes.js
+++ Backend/routes/repaymentsRoutes.js
@@ -651,6 +651,9 @@ async function processRows(sheetData, res) {
         queryDB(`SELECT lan FROM loan_booking_circle_pe WHERE lan IN (?)`, [
           uniqueLANs,
         ]),
+         queryDB(`SELECT lan FROM loan_booking_motion_corp  WHERE lan IN (?)`, [
+          uniqueLANs,
+        ]),
         queryDB(`SELECT lan FROM loan_booking_helium WHERE lan IN (?)`, [
           uniqueLANs,
         ]),
