1f7ef701568bd1a3170bf9b73458def80709a4e8
AACHAL-6088 <dubeyaachal71@gmail.com>
2026-08-26 14:56:17 +0530

    consolidate-mis for claim_cure_buddy

--- Backend/routes/reportRoutes.js
+++ Backend/routes/reportRoutes.js
@@ -212,6 +212,8 @@ function resolveProcedure(rawReportId, rawLender) {
                                               ? "sp_consolidated_mis_report_saswat"
                                               : lender === "sterlion ubl"
                                                 ? "sp_consolidated_mis_report_sterlion_ubl"
+                                              :lender === "claimcurebuddy"
+                                                ? "sp_consolidated_mis_report_claim_cure_buddy"
                                                 : "sp_consolidated_mis_report",
 
     // NEW IRR Report add
