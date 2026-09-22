c53952274ee0b5843bd42ff124563e46a6c84ea0
AACHAL-6088 <dubeyaachal71@gmail.com>
2026-09-22 14:18:00 +0530

    Add QMLApprovedLoans  and update QMLDisburseInitiate

--- Frontend/src/components/QuickMoney/QMLDisburseInitiate.jsx
+++ Frontend/src/components/QuickMoney/QMLDisburseInitiate.jsx
@@ -1,14 +1,13 @@
 
-import React from 'react'
-import LoginActionScreen from '../LoginActionScreen'
 
-const QMLDisburseInitiate = () => {
-  return (
-    // BLApprovedLoans.js
-<LoginActionScreen apiUrl={`/loan-booking/approved-loans?table=loan_booking_quick_money&prefix=QML`}
-      title="Quick Money Disburse Initiate Pending Loans" />
+import React from "react";
+import DisbursedLoansTable from "../DisbursedLoansScreen";
 
-  )
-}
+const QMLDisburseInitiate = () => (
+  <DisbursedLoansTable
+    apiEndpoint="/loan-booking/disbursed-loans?table=loan_booking_quick_money&prefix=QML"
+    title="Quick Money Disbursed Loans"
+  />
+);
 
-export default QMLDisburseInitiate
+export default QMLDisburseInitiate;
